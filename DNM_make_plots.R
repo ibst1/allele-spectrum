874366fe38364f471681b71fabb056bb9e10754b
ibst1 <48456621+ibst1@users.noreply.github.com>
2020-08-09 20:30:40 +0200

    Delete DNM_make_plots.R

--- DNM_make_plots.R
+++
@@ -1,5 +0,0 @@
-source("make_plots.R")
-
-df <- left_join(read_tsv('./outdata/DNM_groups_no.CpG.status.tsv', col_names = FALSE), read_tsv('./outdata/DNM_groups_not.in.CpG.tsv', col_names = FALSE), by='X1') %>% left_join(., read_tsv('./outdata/DNM_groups_in.CpG.tsv', col_names = FALSE), by="X1")
-colnames(df) <- c("X1", "All DNMs", "Non-CpG", "CpG")
-makefig(df, "figures/DNMs.png")
