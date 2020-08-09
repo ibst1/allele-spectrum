d65f1ea43cfd6fe95ba5074833fa825d07429a05
ibst1 <48456621+ibst1@users.noreply.github.com>
2020-08-09 20:28:00 +0200

    Add files via upload

---
+++ primates_extract_groups_all.sh
@@ -0,0 +1,6 @@
+. primates_extract_groups_all_helper.sh './primates/VCFs/SNPs/Gorilla.vcf' > ./outdata/primates_groups.Gorilla_all.tsv
+. primates_extract_groups_all_helper.sh './primates/VCFs/SNPs/Homo.vcf' > ./outdata/primates_groups.Homo_all.tsv
+. primates_extract_groups_all_helper.sh './primates/VCFs/SNPs/Pan_paniscus.vcf' > ./outdata/primates_groups.Pan_paniscus_all.tsv
+. primates_extract_groups_all_helper.sh './primates/VCFs/SNPs/Pan_troglodytes.vcf' > ./outdata/primates_groups.Pan_troglodytes_all.tsv
+. primates_extract_groups_all_helper.sh './primates/VCFs/SNPs/Pongo_abelii.vcf' > ./outdata/primates_groups.Pongo_abelii_all.tsv
+. primates_extract_groups_all_helper.sh './primates/VCFs/SNPs/Pongo_pygmaeus.vcf' > ./outdata/primates_groups.Pongo_pygmaeus_all.tsv
---
+++ primates_extract_groups_fixed.sh
@@ -0,0 +1,6 @@
+. primates_extract_groups_fixed_helper.sh './primates/VCFs/SNPs/Gorilla.vcf' > ./outdata/primates_groups.Gorilla_fixed.tsv
+. primates_extract_groups_fixed_helper.sh './primates/VCFs/SNPs/Homo.vcf' > ./outdata/primates_groups.Homo_fixed.tsv
+. primates_extract_groups_fixed_helper.sh './primates/VCFs/SNPs/Pan_paniscus.vcf' > ./outdata/primates_groups.Pan_paniscus_fixed.tsv
+. primates_extract_groups_fixed_helper.sh './primates/VCFs/SNPs/Pan_troglodytes.vcf' > ./outdata/primates_groups.Pan_troglodytes_fixed.tsv
+. primates_extract_groups_fixed_helper.sh './primates/VCFs/SNPs/Pongo_abelii.vcf' > ./outdata/primates_groups.Pongo_abelii_fixed.tsv
+. primates_extract_groups_fixed_helper.sh './primates/VCFs/SNPs/Pongo_pygmaeus.vcf' > ./outdata/primates_groups.Pongo_pygmaeus_fixed.tsv
